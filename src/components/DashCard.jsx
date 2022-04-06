4138635371f7d19ce036a8c65c2edfad05315ed5
Abhishek Gaur <abhishek.gaur@remotestate.com>
2022-04-06 14:51:58 +0530

    dashboard - complete

--- src/components/DashCard.jsx
+++ src/components/DashCard.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import '../css/dashcard.css'
 
-function DashCard({title,count}) {
+function DashCard({title,count,view,cls,clsh}) {
   return (
     <>
         <div className="dashcard">
             <div className="title">
                 <h2>{title}</h2>
             </div>
-            <div className="numb">
-                    <h1>{count}</h1>
+            <div className= {'numb ' + (cls? cls : '')}>
+                    <h1 className={'count '+ (clsh? clsh : '')} >{count}</h1>
             </div>
-                <button className='vd_btn' >View Details</button>
+              { view ? <button className='vd_btn' >View Details</button>
+              : ''}
         </div>
     </>
   )
